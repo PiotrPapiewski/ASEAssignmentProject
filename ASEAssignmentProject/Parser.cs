f5d0c1427185a121dd39cf62337a1c08126ce4fe
Piotr Papiewski <P.Papiewski9989@student.leedsbeckett.ac.uk>
2022-11-19 15:04:00 +0000

    Fixed error wrongly placed }

--- ASEAssignmentProject/Parser.cs
+++ ASEAssignmentProject/Parser.cs
@@ -11,107 +11,107 @@ namespace ASEAssignmentProject
     {
         Canvass Canvass; //creates an instance of canvass class in parser class
         Bitmap DrawingSurface; //creates an bitmap in parser
-    }
-    public Parser(Bitmap DrawingSurface, Canvass canvass)
-    {
-        this.DrawingSurface = DrawingSurface;
-        this.Canvass = canvass;
-    }
 
-    public void MyParser(string CommandLine)
-    {
-        String Command = CommandLine.ToLower(); //allows commands to be enter using lower case and stores user impunt in command variable
-        String[] strArray = Command.Split(' '); //splits user command based on space between arguments and puts it in strarray
-        String[] Parameters; //creates empty array for parameters
-
-        Command = strArray[0]; //specifies that position 0 in a array is a command
-
-        int[] ParametersInt; //creates empty integer array for parameters
-
-        if (strArray.Length > 1) //check if length of array is more than one
+        public Parser(Bitmap DrawingSurface, Canvass canvass)
         {
-            if (Command.Equals("fill")) //checks if command fill was entered
+            this.DrawingSurface = DrawingSurface;
+            this.Canvass = canvass;
+        }
+
+        public void MyParser(string CommandLine)
+        {
+            String Command = CommandLine.ToLower(); //allows commands to be enter using lower case and stores user impunt in command variable
+            String[] strArray = Command.Split(' '); //splits user command based on space between arguments and puts it in strarray
+            String[] Parameters; //creates empty array for parameters
+
+            Command = strArray[0]; //specifies that position 0 in a array is a command
+
+            int[] ParametersInt; //creates empty integer array for parameters
+
+            if (strArray.Length > 1) //check if length of array is more than one
             {
-                Canvass.SetFill(strArray[1]); //if command fill was entered executes method setfill from canvass
+                if (Command.Equals("fill")) //checks if command fill was entered
+                {
+                    Canvass.SetFill(strArray[1]); //if command fill was entered executes method setfill from canvass
+                }
+                else
+                {
+                    Parameters = strArray[1].Split(','); //Splits user input again this time it splits parameters based on comma,
+                    ParametersInt = new int[Parameters.Length]; //creats new array based on how long paratemetrs array is
+                    for (int i = 0; i < Parameters.Length; i++) //loops through the Parameter array 
+                    {
+                        ParametersInt[i] = Convert.ToInt32(Parameters[i]); //converts into integer and stores in new array ParametersInt
+
+                    } //checks which commands are entered and executes appropriate method
+                    if (Command.Equals("drawline"))
+                    {
+                        Canvass.DrawLine(ParametersInt[0], ParametersInt[1]); //uses canvass method to draw
+
+                    }
+                    else if (Command.Equals("drawsquare"))
+                    {
+                        Canvass.DrawSquare(ParametersInt[0]);
+
+                    }
+                    else if (Command.Equals("drawrectangle"))
+                    {
+                        Canvass.DrawRectangle(ParametersInt[0], ParametersInt[1]);
+
+                    }
+                    else if (Command.Equals("drawcircle"))
+                    {
+                        Canvass.DrawCircle(ParametersInt[0]);
+
+                    }
+                    else if (Command.Equals("drawtriangle"))
+                    {
+                        Canvass.DrawTriangle(ParametersInt[0], ParametersInt[1]);
+
+                    }
+                    else if (Command.Equals("movepen"))
+                    {
+                        Canvass.MovePen(ParametersInt[0], ParametersInt[1]);
+
+                    }
+                }
+
             }
             else
             {
-                Parameters = strArray[1].Split(','); //Splits user input again this time it splits parameters based on comma,
-                ParametersInt = new int[Parameters.Length]; //creats new array based on how long paratemetrs array is
-                for (int i = 0; i < Parameters.Length; i++) //loops through the Parameter array 
+                if (Command.Equals("reset"))
                 {
-                    ParametersInt[i] = Convert.ToInt32(Parameters[i]); //converts into integer and stores in new array ParametersInt
-
-                } //checks which commands are entered and executes appropriate method
-                if (Command.Equals("drawline"))
-                {
-                    Canvass.DrawLine(ParametersInt[0], ParametersInt[1]); //uses canvass method to draw
-
+                    Canvass.Reset();
                 }
-                else if (Command.Equals("drawsquare"))
+                else if (Command.Equals("clear"))
                 {
-                    Canvass.DrawSquare(ParametersInt[0]);
-
+                    Canvass.Clear(DrawingSurface);
                 }
-                else if (Command.Equals("drawrectangle"))
+                else if (Command.Equals("red"))
                 {
-                    Canvass.DrawRectangle(ParametersInt[0], ParametersInt[1]);
-
+                    Canvass.PenColourRed();
                 }
-                else if (Command.Equals("drawcircle"))
+                else if (Command.Equals("blue"))
                 {
-                    Canvass.DrawCircle(ParametersInt[0]);
-
+                    Canvass.PenColourBlue();
                 }
-                else if (Command.Equals("drawtriangle"))
+                else if (Command.Equals("yellow"))
                 {
-                    Canvass.DrawTriangle(ParametersInt[0], ParametersInt[1]);
-
+                    Canvass.PenColourYellow();
                 }
-                else if (Command.Equals("movepen"))
+                else if (Command.Equals("black"))
                 {
-                    Canvass.MovePen(ParametersInt[0], ParametersInt[1]);
-
+                    Canvass.PenColourBlack();
                 }
             }
-
         }
-        else
+
+        public void ProgramWindow(string UserInput) //this method allows multiple compands to be written in InputBox
         {
-            if (Command.Equals("reset"))
+            string[] Commands = UserInput.Split(new string[] { "\n" }, StringSplitOptions.None); //splits commands from inputbox by new line
+            foreach (string Command in Commands)
             {
-                Canvass.Reset();
-            }
-            else if (Command.Equals("clear"))
-            {
-                Canvass.Clear(DrawingSurface);
-            }
-            else if (Command.Equals("red"))
-            {
-                Canvass.PenColourRed();
-            }
-            else if (Command.Equals("blue"))
-            {
-                Canvass.PenColourBlue();
-            }
-            else if (Command.Equals("yellow"))
-            {
-                Canvass.PenColourYellow();
-            }
-            else if (Command.Equals("black"))
-            {
-                Canvass.PenColourBlack();
+                MyParser(Command); //loops and executs commands from MyParser method so they can be used in ProgramWindow method
             }
         }
     }
-
-    public void ProgramWindow(string UserInput) //this method allows multiple compands to be written in InputBox
-    {
-        string[] Commands = UserInput.Split(new string[] { "\n" }, StringSplitOptions.None); //splits commands from inputbox by new line
-        foreach (string Command in Commands)
-        {
-            MyParser(Command); //loops and executs commands from MyParser method so they can be used in ProgramWindow method
-        }
-    }
-
 }
