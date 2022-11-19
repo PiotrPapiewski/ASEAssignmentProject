450fa8a4783796aa762192cc6433d4e2349f116c
Piotr Papiewski <P.Papiewski9989@student.leedsbeckett.ac.uk>
2022-11-19 14:18:28 +0000

    Created Canvass class with graphic, pen, bitmap

--- ASEAssignmentProject/Form1.cs
+++ ASEAssignmentProject/Form1.cs
@@ -12,9 +12,12 @@ namespace ASEAssignmentProject
 {
     public partial class Form1 : Form
     {
+        static Bitmap DrawingSurface = new Bitmap(400, 300); //creating a bitmap and assigning it to the picture box 'DrawingSurface'
+        static Canvass MyCanvass; //creating an instnace of canvass class inside form1 class
         public Form1()
         {
-            InitializeComponent();
+            InitializeComponent(); //method that creates from
+            MyCanvass = new Canvass(Graphics.FromImage(DrawingSurface)); //updates mycanvass to be able to display drawings onto 'DrawingSurface' 
         }
 
         private void CommandLine_KeyDown(object sender, KeyEventArgs e)
