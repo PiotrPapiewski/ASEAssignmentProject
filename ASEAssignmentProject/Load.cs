4b998576800b1baf0eef393fb51f776cb631538c
Piotr Papiewski <P.Papiewski9989@student.leedsbeckett.ac.uk>
2022-12-15 09:23:43 +0000

    added save and load function to the program

---
+++ ASEAssignmentProject/Load.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ASEAssignmentProject
+{
+    public class Load
+    {
+
+        public void LoadFile(RichTextBox rtb)
+        {
+
+            OpenFileDialog ofd = new OpenFileDialog();
+            if (ofd.ShowDialog() == DialogResult.OK)
+                rtb.LoadFile(ofd.FileName);
+
+        }
+
+    }
+}
